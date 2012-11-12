d0f5dd2b070fb93af3cab8b2146b9428672af96f
paschelino <paschelino@gmail.com>
2012-11-13 00:10:03 +0100

    fix comparability issue with empty paths

--- src/main/java/de/cosmicsand/webtools/path/Path.java
+++ src/main/java/de/cosmicsand/webtools/path/Path.java
@@ -3,7 +3,7 @@ package de.cosmicsand.webtools.path;
 import static org.apache.commons.lang.StringUtils.isEmpty;
 import static org.apache.commons.lang.StringUtils.isNotEmpty;
 
-public class Path implements Comparable {
+public class Path implements Comparable<Path> {
     public static final Path EMPTY_PATH = new Path();
     private final String rawPath;
 
@@ -22,21 +22,19 @@ public class Path implements Comparable {
 
     @Override
     public boolean equals(Object obj) {
-        if (obj instanceof Path && isNotEmpty(rawPath))
+        if (obj instanceof Path)
             return rawPath.equals(((Path) obj).rawPath);
         return false;
     }
 
     @Override
     public int hashCode() {
-        if (isEmpty(rawPath))
-            return 0;
         return rawPath.hashCode();
     }
 
     @Override
-    public int compareTo(Object arg0) {
-        Path other = (Path) arg0;
+    public int compareTo(Path other) {
+        // Path other = (Path) arg0;
         if (isEmpty(rawPath) && isEmpty(other.rawPath))
             return 0;
         else if (isNotEmpty(rawPath) && isEmpty(other.rawPath))
